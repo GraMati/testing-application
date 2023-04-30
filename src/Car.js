3781f8250fbe61e88129932ee7b7a30ce8f0dec9
Mateusz <46197224+GraMati@users.noreply.github.com>
2023-04-30 16:48:35 +0200

    rename title (#4)
    Co-authored-by: Mateusz Majewski <majewskimateusz89@gmail.com>

--- src/Car.js
+++ src/Car.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Car = (props) => {
     const carImage = props.image;
-    const carName = props.name || "N/A";
+    const carName = props.name || "Not available";
     const carDescription = props.description;
 
     return (

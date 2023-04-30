7512044b06720b97457962264ccfd9a810a96814
Mateusz <46197224+GraMati@users.noreply.github.com>
2023-04-30 16:15:02 +0200

    Component props (#3)
    * Component Props
    
    * Changed declaration of component
    
    ---------
    
    Co-authored-by: mateusz m <majewskimateusz89@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Car />
+        <Car
+          image="https://www.tuningblog.eu/wp-content/uploads/2022/06/Mansory-Bentley-Bentayga-Speed-Feroza-Edition-2022-Tuning-5.jpg"
+          name="Bentley Bentayga"
+          description="SUV klasy premium."
+        />
+        <Car
+          image="https://samochodyelektryczne.org/img/tesla_model_s/1/tesla_model_s_162.jpg"
+          description="Elektryczny samochód klasy premium."
+        />
+        <Car
+          image="https://www.carscoops.com/wp-content/uploads/2019/11/1967-Ford-Mustang-Eleanor.jpg"
+          name="Ford Mustang Shelby GT500 Eleanor 1967"
+          description="Samochód na bazie Forda Mustanga zaprojektowany w latach 60 XX wieku w stylu muscle car."
+        />
       </header>
     </div>
   );
 }
-
-export default App;
